028e6d48a9488328cf020e1fc94ceb81a29d82a4
XinZhang <zhanxin_chn@126.com>
2018-10-08 17:09:46 +0800

    some fix

--- rds_tools/utils/read_sqls.py
+++ rds_tools/utils/read_sqls.py
@@ -6,11 +6,21 @@
 
 
 import pandas as pd
-from .config_parser import mysql_conf
+from sqlalchemy.sql import select,and_
 
+def query_frame_from_table(table, column_list = None, conditions = None):
+    if column_list is None:
+        select_param = [table]
+    else:
+        assert isinstance(column_list, list), 'Expected type of column_list is list, given: %s'%(type(column_list))
+        select_param = [table.c.get(s) for s in column_list]
 
-def read_raw_sql(raw_sql, engine = mysql_conf.production_engine()):
-    return pd.read_sql(raw_sql, engine)
+    if conditions is None:
+        return pd.read_sql(select(select_param), table.bind)
 
-
-
+    else:
+        return pd.read_sql(
+            select(select_param).where(
+                and_(*conditions)
+            ), table.bind
+        )
